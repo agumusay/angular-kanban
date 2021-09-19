afe5d6660a2cdb8dd9f927eb43520b64466c0aab
Ali San Gumusay <alisan@pop-os.localdomain>
2021-09-20 00:18:35 +0200

    lazy loaded feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: HomePageComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  {
+    path: 'login',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
