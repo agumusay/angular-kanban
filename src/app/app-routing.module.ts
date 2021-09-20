d7e2686c24270317d6e0f93159de2233fad9e6fc
Ali San Gumusay <alisan@pop-os.localdomain>
2021-09-20 04:42:46 +0200

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
+import { AuthGuard } from './user/auth.guard';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
   {
     path: 'login',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
